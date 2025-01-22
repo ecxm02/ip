e731cc17415b4920ea4aaa9f4ab24d360bada78b
ecxm02 <familychan1@gmail.com>
2025-01-22 11:08:06 +0800

    Week 2 (Level 0 IP, Rename Greet Exit)

---
+++ src/main/java/Juke.java
@@ -0,0 +1,7 @@
+public class Juke {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Juke");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
