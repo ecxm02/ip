765d297dc066dedf27aa3a0825928bd0c922450f
ecxm02 <familychan1@gmail.com>
2025-01-31 15:13:47 +0800

    Add Echo
    Added simple reply functionality to output the input and end the program when a "bye" is detected

--- src/main/java/Juke.java
+++ src/main/java/Juke.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Juke {
+    static boolean isDone = false;
+
+    static void replyText(String text) {
+        System.out.println("---------------------------------------------------");
+        if (text.equals("bye")) {
+            isDone = true;
+            System.out.println("Bye! See you again :)");
+        } else {
+            System.out.println(text);
+        }
+        System.out.println("---------------------------------------------------");
+    }
+
     public static void main(String[] args) {
+        System.out.println("---------------------------------------------------");
         System.out.println("Hello! I'm Juke");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("SAY SOMETHING!");
+        System.out.println("---------------------------------------------------");
+
+        Scanner input = new Scanner(System.in);
+        String text;
+
+        while (!isDone) {
+            text = input.nextLine();
+            replyText(text);
+        }
+
+        input.close();
     }
 }
