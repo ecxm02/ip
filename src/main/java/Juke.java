e6ea503300a8f602226f7c05b8bd731ec76cf0cc
ecxm02 <familychan1@gmail.com>
2025-01-31 17:15:53 +0800

    Amend to follow coding standards
    Removed unused library and added comments

--- src/main/java/Juke.java
+++ src/main/java/Juke.java
@@ -1,17 +1,16 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Juke {
-    static boolean isCompleted = false;
-    static Task[] list = new Task[100];
-    static int currentListPosition = 0;
+    static boolean isCompleted = false; //a master flag so that main knows when to end the program
+    static Task[] list = new Task[100]; //array of Task
+    static int currentListPosition = 0; //keep track of where to add new tasks
 
-    static void endProgram() {
+    static void endProgram() { //sets the isCompleted flag to true so that the loop ends, also prints end message
         isCompleted = true;
         System.out.println("Bye! See you again :)");
     }
 
-    static void listTask() {
+    static void listTask() { //prints out the tasks and marks them appropriately
         for (int i = 0; i < currentListPosition; i++) {
             System.out.print((i + 1));
 
@@ -23,14 +22,14 @@ public class Juke {
         }
     }
 
-    static void addTask(String text) {
+    static void addTask(String text) { //adds tasks into the array
         list[currentListPosition] = new Task(text);
         list[currentListPosition].setTask(text);
         currentListPosition++;
         System.out.println("added: " + text);
     }
 
-    static void markTask(String text) {
+    static void markTask(String text) { //marks a task as completed
         int spaceIndex = text.indexOf(" ");
         String taskNumber = text.substring(spaceIndex + 1);
         int taskIndex = Integer.parseInt(taskNumber) - 1;
@@ -39,7 +38,7 @@ public class Juke {
         System.out.println("[X] " + list[taskIndex].getTask());
     }
 
-    static void unmarkTask(String text) {
+    static void unmarkTask(String text) { //marks a task as uncompleted
         int spaceIndex = text.indexOf(" ");
         String taskNumber = text.substring(spaceIndex + 1);
         int taskIndex = Integer.parseInt(taskNumber) - 1;
@@ -49,7 +48,7 @@ public class Juke {
     }
 
 
-    static void replyText(String text) {
+    static void replyText(String text) { //uses a swtich and if else statments to execute appropriate methods for the input
         System.out.println("---------------------------------------------------");
 
         switch (text) {
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,6 @@
 public class Task {
-    protected String task;
-    protected boolean isDone;
+    protected String task; //actual content of task
+    protected boolean isDone; //boolean flag to check if its completed or uncompleted
 
     public Task(String description) {
         this.task = null;
