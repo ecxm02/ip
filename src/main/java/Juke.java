88eba57d83db7550ed481eba11a08d99449ed8d2
ecxm02 <familychan1@gmail.com>
2025-01-31 15:32:10 +0800

    Add task and list
    Add the ability to add tasks and list out all tasks

--- src/main/java/Juke.java
+++ src/main/java/Juke.java
@@ -2,15 +2,37 @@ import java.util.Scanner;
 
 public class Juke {
     static boolean isDone = false;
+    static String[] list = new String[100];
+    static int currentListPosition = 0;
+
+    static void endProgram() {
+        isDone = true;
+        System.out.println("Bye! See you again :)");
+    }
+
+    static void listTask() {
+        for (int i = 0; i < list.length && list[i] != null; i++) {
+            System.out.println((i + 1) + ". " + list[i]);
+        }
+    }
+
+    static void addTask(String text) {
+        list[currentListPosition] = text;
+        currentListPosition++;
+        System.out.println("added: " + text);
+    }
 
     static void replyText(String text) {
         System.out.println("---------------------------------------------------");
+
         if (text.equals("bye")) {
-            isDone = true;
-            System.out.println("Bye! See you again :)");
+            endProgram();
+        } else if (text.equals("list")) {
+            listTask();
         } else {
-            System.out.println(text);
+            addTask(text);
         }
+
         System.out.println("---------------------------------------------------");
     }
 
